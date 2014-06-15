78dec1b53fde5d945b8829bff8ef7b4c42eed830
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-06-15 02:41:27 -0300

    Using anishathalye/dotbot
    This is a very nice project that allow an easier home bootstraping using
    a clever Python script.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CONFIG="install.conf.json"
+DOTBOT_DIR="dotbot"
+
+DOTBOT_BIN="bin/dotbot"
+PYTHON_BIN="python2"
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${BASEDIR}"
+git submodule update --init --recursive ${DOTBOT_DIR}
+
+${PYTHON_BIN} "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" $@
