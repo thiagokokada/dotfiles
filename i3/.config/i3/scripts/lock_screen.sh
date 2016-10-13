2b9bb3a39fd4ead89470fe47db60d0e8d4e0cb24
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-10-12 23:19:12 -0300

    Use i3lock-fancy as lock screen

--- i3/.config/i3/scripts/lock_screen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-TEMP_FILE='/tmp/lockscreen.png'
-
-trap "rm -f "$TEMP_FILE"; exit" SIGHUP SIGINT SIGTERM
-
-scrot "$TEMP_FILE"
-convert "$TEMP_FILE" -scale 10% -scale 1000% "$TEMP_FILE"
-i3lock -I 5 -eti "$TEMP_FILE"
