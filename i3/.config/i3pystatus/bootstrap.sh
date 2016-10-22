4225a36ab07563e66e8c35018d888ce3f0028328
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-10-22 21:43:59 -0200

    Migrate to zsh-autoenv

--- i3/.config/i3pystatus/bootstrap.sh
+++ i3/.config/i3pystatus/bootstrap.sh
@@ -4,20 +4,13 @@
 # Run this script to boostrap i3pystatus virtual environment #
 ##############################################################
 
-BASE_PATH=`pwd`
+BASE_PATH=`dirname $0`
 VENV_PATH="${BASE_PATH}/venv"
 VENV_BIN_PATH="${VENV_PATH}/bin"
-PY_LIB_PATH="lib/python3.[0-9]/site-packages"
-GOBJECT_USR_PATH="/usr/${PY_LIB_PATH}/gi"
 
 if [ ! -d "${VENV_PATH}" ]; then
     echo "Creating virtualenv and installing dependencies..."
-    virtualenv3 "${VENV_PATH}"
+    virtualenv3 --system-site-packages "${VENV_PATH}"
     ${VENV_BIN_PATH}/pip install -r "${BASE_PATH}/requirements.txt"
-    if [ -f ${GOBJECT_USR_PATH}/__init__.py ]; then
-        ln -s ${GOBJECT_USR_PATH} ${VENV_PATH}/${PY_LIB_PATH}
-    else
-        echo "WARNING: python-gobject not installed! No notifications." 1>&2
-    fi
     echo "Finished ipy3status setup. Please restart i3."
 fi
