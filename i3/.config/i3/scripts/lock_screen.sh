c237983d7f45db43f282e086003327072f03fe8d
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-11-01 10:41:30 -0200

    Speed up lock script even fast by using x11grab, change ws

--- i3/.config/i3/scripts/lock_screen.sh
+++ i3/.config/i3/scripts/lock_screen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 TEMP_FILE=`mktemp --suffix '.png'`
+RESOLUTION=`xdpyinfo | awk '/dimensions/{print $2}'`
 DPMS_VALUES=`xset q | awk 'BEGIN{FPAT="[0-9]+"} /Standby.*Suspend.*Off/{print $1, $2, $3}'`
 SCREEN_TIMEOUT=5
 
@@ -11,8 +12,8 @@ clean_up() {
 
 trap clean_up SIGHUP SIGINT SIGTERM
 
-scrot "${TEMP_FILE}"
-ffmpeg -loglevel quiet -y -i "${TEMP_FILE}" -vf 'gblur=sigma=8' "${TEMP_FILE}"
+# scrot "${TEMP_FILE}"
+ffmpeg -loglevel quiet -y -s "${RESOLUTION}" -f x11grab -i "${DISPLAY}" -vframes 1 -vf 'gblur=sigma=8' "${TEMP_FILE}"
 xset +dpms dpms "${SCREEN_TIMEOUT}" "${SCREEN_TIMEOUT}" "${SCREEN_TIMEOUT}"
 i3lock -I "${SCREEN_TIMEOUT}" -nei "${TEMP_FILE}"
 clean_up
