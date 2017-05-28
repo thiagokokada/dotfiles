d03a344343dd24c2ca4baf0048fb233ac72061b1
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-05-27 21:12:09 -0300

    python,ruby: Create separate modules for those languages

--- python/.zshrc.d/python.zsh
+++ python/.zshrc.d/python.zsh
@@ -1,1 +1,10 @@
 export PYTHONSTARTUP="$HOME/.pythonrc.py"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+if [ ! -d "$PYENV_ROOT" ]; then
+  git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
+  git clone https://github.com/pyenv/pyenv-virtualenv.git "$PYENV_ROOT/plugins/pyenv-virtualenv"
+fi
+
+ eval "$(pyenv init - zsh)"
---
+++ ruby/.zshrc.d/ruby.zsh
@@ -0,0 +1,18 @@
+export RBENV_ROOT="$HOME/.rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+
+if [ ! -d "$RBENV_ROOT" ]; then
+  git clone https://github.com/rbenv/rbenv.git "$RBENV_ROOT"
+  git clone https://github.com/rbenv/ruby-build.git "$RBENV_ROOT/plugins/ruby-build"
+fi
+
+eval "$(rbenv init --no-rehash - zsh)"
+
+alias be="bundle exec"
+alias bl="bundle list"
+alias bp="bundle package"
+alias bo="bundle open"
+alias bout="bundle outdated"
+alias bu="bundle update"
+alias bi="bundle install"
+alias bcn="bundle clean"
