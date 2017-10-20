729a35732e4187e3a765bfaa5846466f76e74224
Thiago Kenji Okada <thiagokokada@gmail.com>
2017-10-20 21:08:00 -0200

    sway: Initial swaywm config
    Based on my current i3 configuration. Also it depends on i3 config to be
    stow'ed, for now.

---
+++ sway/.tty2_login.zsh
@@ -0,0 +1,9 @@
+# Enable integration between GTK2 and Qt5
+export QT_QPA_PLATFORMTHEME=gtk2
+
+# Keyboard layout config
+export XKB_DEFAULT_LAYOUT=br,us
+export XKB_DEFAULT_VARIANT=abnt2,intl
+export XKB_DEFAULT_OPTIONS=grp:alt_shift_toggle,
+
+sway
