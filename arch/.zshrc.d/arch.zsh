0ddd009ea039ef1cce8e9537d71280cbebd30e43
Thiago Kenji Okada <thiagokokada@gmail.com>
2018-10-07 12:31:59 -0300

    arch: Update zit lastly

--- arch/.zshrc.d/arch.zsh
+++ arch/.zshrc.d/arch.zsh
@@ -1,7 +1,7 @@
 # Command to update everything
 archup() {
-  zit-update
   (( $+commands[yay] )) && yay
   (( $+commands[flatpak] )) && flatpak update
   (( $+commands[nvim] )) && nvim -c PlugUpdate -c qall
+  zit-update
 }
