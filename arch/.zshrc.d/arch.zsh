b61416f3fe702ef15037d42695378fc8ac804cd5
Thiago Kenji Okada <thiagokokada@gmail.com>
2018-10-07 12:25:21 -0300

    arch: Improve archup alias

--- arch/.zshrc.d/arch.zsh
+++ arch/.zshrc.d/arch.zsh
@@ -1,1 +1,7 @@
-alias archup="yay && flatpak update"
+# Command to update everything
+archup() {
+  zit-update
+  (( $+commands[yay] )) && yay
+  (( $+commands[flatpak] )) && flatpak update
+  (( $+commands[nvim] )) && nvim -c PlugUpdate -c qall
+}
