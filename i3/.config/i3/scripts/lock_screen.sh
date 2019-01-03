e46088bbe92f0bf01da43c66f22bdb4edd0acc73
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-01-03 10:21:16 -0200

    i3: Use only screen timeout timer in DPMS

--- i3/.config/i3/scripts/lock_screen.sh
+++ i3/.config/i3/scripts/lock_screen.sh
@@ -2,17 +2,16 @@
 
 TEMP_FILE=$(mktemp --suffix '.png')
 RESOLUTION=$(xdpyinfo | awk '/dimensions/{print $2}')
-DPMS_VALUES=$(xset q | awk 'BEGIN{FPAT="[0-9]+"} /Standby.*Suspend.*Off/{print $1, $2, $3}')
-SCREEN_TIMEOUT=5
+OLD_STANDBY=$(xset q | awk 'BEGIN {FPAT="[0-9]+"} /Standby/{print $1}')
+NEW_STANDBY=5
 
 clean_up() {
     rm -f "${TEMP_FILE}"
-    # shellcheck disable=SC2086
-    xset dpms ${DPMS_VALUES}
+    xset dpms "${OLD_STANDBY}"
 }
 
 trap clean_up HUP INT TERM EXIT
 
 ffmpeg -loglevel quiet -y -s "${RESOLUTION}" -f x11grab -i "${DISPLAY}" -vframes 1 -vf 'gblur=sigma=8' "${TEMP_FILE}"
-xset +dpms dpms "${SCREEN_TIMEOUT}" "${SCREEN_TIMEOUT}" "${SCREEN_TIMEOUT}"
+xset +dpms dpms "${NEW_STANDBY}"
 i3lock -nei "${TEMP_FILE}"
