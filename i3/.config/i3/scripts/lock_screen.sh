8797e9f4a9e526e1de42f1e17371d5f45d989184
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-05 12:05:13 -0200

    i3: Improve lock_screen.sh script to handle music player

--- i3/.config/i3/scripts/lock_screen.sh
+++ i3/.config/i3/scripts/lock_screen.sh
@@ -1,17 +1,49 @@
-#!/bin/sh
+#!/bin/bash
+
+set -euo pipefail
 
 TEMP_FILE=$(mktemp --suffix '.png')
-RESOLUTION=$(xdpyinfo | awk '/dimensions/{print $2}')
 OLD_STANDBY=$(xset q | awk 'BEGIN {FPAT="[0-9]+"} /Standby/{print $1}')
 NEW_STANDBY=5
+PLAY=0
+
+pause() {
+    local status;
+    
+    status="$(playerctl status || true)"
+    playerctl -a pause 2> /dev/null || true
+
+    if [[ "${status}" == "Playing" ]]; then
+        PLAY=1
+    fi
+}
+
+resume() {
+    if [[ "${PLAY}" -eq 1 ]]; then
+        playerctl play || true
+    fi
+}
+
+take_screenshot() {
+    local resolution;
+    
+    resolution=$(xdpyinfo | awk '/dimensions/{print $2}')
+    ffmpeg -loglevel quiet -y -s "${resolution}" -f x11grab -i "${DISPLAY}" -vframes 1 -vf 'gblur=sigma=8' "${TEMP_FILE}"
+}
+
+prepare() {
+    take_screenshot
+    xset +dpms dpms "${NEW_STANDBY}"
+    pause
+}
 
 clean_up() {
     rm -f "${TEMP_FILE}"
     xset dpms "${OLD_STANDBY}"
+    resume
 }
 
 trap clean_up HUP INT TERM EXIT
 
-ffmpeg -loglevel quiet -y -s "${RESOLUTION}" -f x11grab -i "${DISPLAY}" -vframes 1 -vf 'gblur=sigma=8' "${TEMP_FILE}"
-xset +dpms dpms "${NEW_STANDBY}"
+prepare
 i3lock -nei "${TEMP_FILE}"
