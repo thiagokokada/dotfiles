dc3a4b16bba9179f550969c943e168e47d644dbd
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-05 22:24:10 -0200

    i3: Improve pavol.sh script

--- i3/.config/i3/scripts/pavol.sh
+++ i3/.config/i3/scripts/pavol.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-ACTIVESYNC=$(pacmd list-sinks | grep \* | awk '{print $3}')
+ACTIVESYNC=$(pacmd list-sinks | awk '/*/{print $3}')
+PERCENT="${2:-5}"
 
 usage() {
-    echo 'Usage: pavol [OPTION]
+    echo 'Usage: pavol [OPTION] [PERCENT]
 
 Find default PulseAudio sink, and raise/lower/mute it.
 
-raise   Raise volume in default sink by 5%
-lower   Lower volume in default sink by 5%
+raise   Raise volume in default sink by PERCENT
+        Defaults to 5%
+lower   Lower volume in default sink by PERCENT
+        Defaults to 5%
 mute    Mute default sink
 '
 }
 
 case "$1" in
     raise)
-        pactl set-sink-volume "$ACTIVESYNC" +5%
+        pactl set-sink-volume "$ACTIVESYNC" +"${PERCENT}"%
         ;;
     lower)
-        pactl set-sink-volume "$ACTIVESYNC" -5%
+        pactl set-sink-volume "$ACTIVESYNC" -"${PERCENT}"%
         ;;
     mute)
         pactl set-sink-mute "$ACTIVESYNC" toggle
@@ -26,4 +29,4 @@ case "$1" in
     *)
         usage
         ;;
- esac
+esac
