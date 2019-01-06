6b31241abc97f5ed814d6c95ea66f68da98122a7
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-06 14:44:26 -0200

    i3: Go back to Window Title in killallx.sh, again

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -3,7 +3,7 @@
 kill_loop() {
     # Run the kill loop for 50*0.1s=5s
     for _ in $(seq 1 50); do
-        if wmctrl -l | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ci; then
+        if wmctrl -l | awk '{ print substr($0, index($0, $4)) }' | xargs -P0 -L1 wmctrl -c; then
             break
         fi
         sleep 0.1
