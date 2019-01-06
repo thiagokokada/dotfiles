5878719b807efe7b7c9221159eae6e4c6f327c60
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-06 12:24:22 -0200

    i3: Added a loop in killallx.sh

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-wmctrl -l | awk '{ print substr($0, index($0, $4)) }' | xargs -P0 -L1 wmctrl -c
-sleep 0.5
+kill_loop() {
+    # Run the kill loop for 50*0.1s=5s
+    for _ in $(seq 1 50); do
+        if wmctrl -l | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ci; then
+            break
+        fi
+        sleep 0.1
+    done
+}
+
+kill_loop
