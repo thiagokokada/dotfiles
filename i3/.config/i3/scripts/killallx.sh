87104f1de31cbc935fcaba9ee3b2ba1648b42ea4
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-06 14:57:28 -0200

    i3: Go back to hex in wmctrl, however separate params

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -3,7 +3,9 @@
 kill_loop() {
     # Run the kill loop for 50*0.1s=5s
     for _ in $(seq 1 50); do
-        if wmctrl -l | awk '{ print substr($0, index($0, $4)) }' | xargs -P0 -L1 wmctrl -c; then
+        # In wmctrl, -i argument must be set separate from -c or strange things
+        # will happen, at least in version 1.07
+        if wmctrl -l | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -i -c; then
             break
         fi
         sleep 0.1
