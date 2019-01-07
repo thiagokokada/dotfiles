cafeac7cc33159fec7f732e7e7b998f55e7583cf
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-01-07 17:29:32 -0200

    _nixos: Use leiningen 2.7.1

--- _nixos/etc/nixos/nubank-configuration.nix
+++ _nixos/etc/nixos/nubank-configuration.nix
@@ -5,9 +5,23 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    (leiningen.overrideAttrs(oldAttrs: rec {
+      pname = "leiningen";
+      version = "2.7.1";
+      name = "${pname}-${version}";
+
+      src = fetchurl {
+        url = "https://raw.github.com/technomancy/leiningen/${version}/bin/lein-pkg";
+        sha256 = "0rmshl4xchf3blwvar4q9dpxm9xznn3yzas4vwxqiq3yhapgqkn0";
+      };
+
+      jarsrc = fetchurl {
+        url = "https://github.com/technomancy/leiningen/releases/download/${version}/${name}-standalone.zip";
+        sha256 = "0ivwb1qlxs1hyical0fjgavm9wfkw3f10sk67p5g2p5lpf4pxp1d";
+      };
+    }))
     aws
     kubectl
-    leiningen
     minikube
     nodejs-10_x
     openfortivpn
