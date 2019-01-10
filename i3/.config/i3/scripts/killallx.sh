82e0bbb5870b2f22428c2275a13891cfbca3a74b
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-01-10 00:35:17 -0200

    i3: Let's give killallx.sh a new chance

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-kill_loop() {
-    # Run the kill loop for 10*0.5s=5s
-    for _ in $(seq 1 10); do
-        # In wmctrl, -i argument must be set separate from -c or strange things
-        # will happen, at least in version 1.07
-        if wmctrl -l | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -i -c; then
-            break
-        fi
-        sleep 0.5
-    done
-}
-
-kill_loop
+wmctrl -l | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -i -c
+sleep 1
