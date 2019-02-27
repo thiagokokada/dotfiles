bf6ba2fecdec9892bfb74cd21015fa352da7a10a
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-02-27 11:35:53 -0300

    Simplify killallx.sh script

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-OPEN_WINDOWS=$(wmctrl -lp)
-
-echo ${OPEN_WINDOWS} | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
-
-for i in {1..50}; do
-    ps -p $(echo ${OPEN_WINDOWS} | awk '{ print $3 }' | paste -s -d, -)
-    if [[ ${?} != 0 ]]; then
-        break
-    fi
+i3-msg '[class=".*"] kill'
+        
+while [[ $(wmctrl -l | wc -c) -ne 0 ]]; do
     sleep 0.1
 done
