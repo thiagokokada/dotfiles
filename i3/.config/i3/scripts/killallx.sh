69c02ae974dbb0ac1274466c5d8ddbb619df0b38
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-03-11 21:42:09 -0300

    i3: Improve killallx.sh

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+PIDS=$(wmctrl -lp | awk '{ print $3 }' | sort -u)
+
 i3-msg '[class=".*"] kill'
 
-while [ "$(wmctrl -l | wc -c)" -ne 0 ]; do
+while true; do
+    ALL_KILLED=1
+
+    for pid in ${PIDS}; do
+        kill -0 "${pid}" && ALL_KILLED=0
+    done
+
+    [ "${ALL_KILLED}" -eq 1 ] && break
+
     sleep 0.1
 done
