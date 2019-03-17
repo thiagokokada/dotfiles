88c327eba8bac54b4ef0185215e31226736f8956
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-03-17 12:18:26 -0300

    sway: Force Wayland in some apps

--- sway/.tty2_login.zsh
+++ sway/.tty2_login.zsh
@@ -1,5 +1,21 @@
-# Enable integration between GTK2 and Qt5
+# Enable GNOME support to Wayland (not everything works well)
+# export GDK_BACKEND=wayland
+# export CLUTTER_BACKEND=wayland
+
+# Enable Qt5 support to Wayland
+export QT_QPA_PLATFORM=wayland-egl
 export QT_QPA_PLATFORMTHEME=gtk2
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+# Enable EFL support to Wayland
+export ECORE_EVAS_ENGINE=wayland_egl
+export ELM_ENGINE=wayland_egl
+
+# Enable SDL support to Wayland
+export SDL_VIDEODRIVER=wayland
+
+# Fix Java applications
+export _JAVA_AWT_WM_NONREPARENTING=1
 
 # Keyboard layout config
 export XKB_DEFAULT_LAYOUT=us,br
