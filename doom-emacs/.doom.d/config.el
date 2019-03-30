ac96e0ca299c1e66acd63944e7f847552d0f5f78
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-03-29 22:56:52 -0300

    doom-emacs: Remove terminal fixes

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -2,17 +2,6 @@
 
 ;; Place your private configuration here
 
-;; Emacs fix for terminal sessions
-(when (not window-system) ;; Only use in tty-sessions.
-    (progn
-        (defvar arrow-keys-map (make-sparse-keymap) "Keymap for arrow keys")
-        (define-key esc-map "[" arrow-keys-map)
-        (define-key arrow-keys-map "A" 'previous-line)
-        (define-key arrow-keys-map "B" 'next-line)
-        (define-key arrow-keys-map "C" 'forward-char)
-        (define-key arrow-keys-map "D" 'backward-char))
-    (xterm-mouse-mode 1))
-
 ;; Theme
 (require 'doom-themes)
 (load-theme 'doom-spacegrey t)
