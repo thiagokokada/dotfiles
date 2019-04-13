7de4140ddb9dda41f45769526d250667df992cdf
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-04-12 23:39:23 -0300

    i3: Improve killallx.sh

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-OPEN_WINDOWS=$(wmctrl -lp)
+WIN_LIST=$(wmctrl -lp)
+WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | paste -s -d, -)
 
-echo ${OPEN_WINDOWS} | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
+echo "${WIN_LIST}" | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
 
-for _ in {1..50}; do
-    ps -p $(echo ${OPEN_WINDOWS} | awk '{ print $3 }' | paste -s -d, -)
-    if [[ ${?} != 0 ]]; then
-        break
-    fi
+(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "killall -u ${USER}") &
+
+while true; do
+    ps -p "${WIN_PIDS}" || break
     sleep 0.1
 done
