7a6cde4119a59ce2605a4633bd39c86ad6f2d339
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-04-14 17:17:23 -0300

    i3: Use pkill in killallx.sh

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -5,7 +5,7 @@ WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | paste -s -d, -)
 
 echo "${WIN_LIST}" | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
 
-(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "killall -u ${USER}") &
+(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "pkill -u ${EUID}") &
 
 while true; do
     ps -p "${WIN_PIDS}" || break
