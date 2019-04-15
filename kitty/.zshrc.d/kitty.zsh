b9515089ad6bac381616445801b48381d39fa44d
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-04-14 23:56:36 -0300

    kitty: Speed-up ZSH startup a lot by not calling kitty

--- kitty/.zshrc.d/kitty.zsh
+++ kitty/.zshrc.d/kitty.zsh
@@ -1,6 +1,15 @@
+_kitty() {
+  local src
+  # Send all words up to the word the cursor is currently on
+  src=$(printf "%s " "${(@)words[1,$CURRENT]}" | kitty +complete zsh)
+  if [[ $? == 0 ]]; then
+    eval ${src}
+  fi
+}
+
 if [[ $TERM = "xterm-kitty" ]]; then
   alias icat="kitty +kitten icat"
   alias sshk="kitty +kitten ssh"
   alias d="kitty +kitten diff"
-  source <(kitty + complete setup zsh)
+  compdef _kitty kitty
 fi
