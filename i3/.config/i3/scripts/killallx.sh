f8c0f394a66aba8146acaf52071d5e45dc29de40
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-04-20 12:36:17 -0300

    i3: Improve killallx.sh script

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 WIN_LIST=$(wmctrl -lp)
-WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | paste -s -d, -)
+WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | sort -u | paste -d' ' -)
 
 echo "${WIN_LIST}" | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
 
-(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "pkill -u ${EUID}") &
+(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "kill ${WIN_PIDS}") &
 
 while true; do
-    ps -p "${WIN_PIDS}" || break
+    ps -p ${WIN_PIDS} || break
     sleep 0.1
 done
