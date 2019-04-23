45acd7ce0f403cbb6080fdea639d5e78a6339364
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-04-22 21:32:19 -0300

    tmux: Added more plugins, added vi-based copy keys

--- tmux/.zshrc.d/tmux.zsh
+++ tmux/.zshrc.d/tmux.zsh
@@ -1,6 +1,8 @@
 ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
-  zit-in "https://github.com/tmux-plugins/tmux-sensible" "tmux-sensible"
-ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
   zit-in "https://github.com/seebi/tmux-colors-solarized" "tmux-colors-solarized"
-
-alias tmux="tmux -2"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
+  zit-in "https://github.com/tmux-plugins/tmux-pain-control" "tmux-pain-control"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
+  zit-in "https://github.com/tmux-plugins/tmux-prefix-highlight" "tmux-prefix-highlight"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
+  zit-in "https://github.com/tmux-plugins/tmux-sensible" "tmux-sensible"
