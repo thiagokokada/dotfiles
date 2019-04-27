9f12268d7a434cdfc643b1737621aa0eed55b7b9
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-04-27 13:03:32 -0300

    i3: Fix killallx.sh script, add "Kill them!" button

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 WIN_LIST=$(wmctrl -lp)
-WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | sort -u | paste -d' ' -)
+WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | sort -u | paste -s -d' ' -)
 
 echo "${WIN_LIST}" | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
 
-(i3-nagbar -m "Waiting all programs to exit..." -B "Finish them!" "kill ${WIN_PIDS}") &
+(i3-nagbar -m "Waiting all programs to exit..." \
+    -b "Kill them!" "kill -9 ${WIN_PIDS}" \
+    -B "Finish them!" "kill ${WIN_PIDS}") &
 
 while true; do
     ps -p ${WIN_PIDS} || break
