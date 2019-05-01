078db4b38cb75a88363d559f4b3caeecdba4be66
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-05-01 13:58:25 -0300

    nixos: Added new nixos-clean-up, remove nix-shell

--- nixos/.zshrc.d/nixos.zsh
+++ nixos/.zshrc.d/nixos.zsh
@@ -1,10 +1,10 @@
 export NIX_HOME="${DOTFILES_PATH}/_nixos"
 
-alias nix-shell-fhs="nix-shell ${NIX_HOME}/fhs-shell.nix"
-
 if [[ -n "${commands[fzf-share]}" ]]; then
   source "$(fzf-share)/key-bindings.zsh"
 fi
+
+alias nixos-clean-up="sudo -- sh -c 'nix-collect-garbage -d && nixos-rebuild boot'"
 
 nixos-copy-etc() {
   diff --color=auto -r "${NIX_HOME}/etc/nixos" /etc/nixos/
