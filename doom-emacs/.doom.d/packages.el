298d2dc7669a9e9965765e77d18331d1ca35044e
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-05-20 13:35:04 -0300

    doom-emacs: Improve package pinning

--- doom-emacs/.doom.d/packages.el
+++ doom-emacs/.doom.d/packages.el
@@ -5,8 +5,16 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
-(package! cider :recipe (:fetcher github :repo "clojure-emacs/cider" :commit "200c88adb1314b5811ba749af42ffb6817c1ca1b" :files ("*.el")))
-(package! clj-refactor :recipe (:fetcher github :repo "clojure-emacs/clj-refactor.el" :commit "3d5d1fbf28bfcc00f917cd96d6784968dcbbc962"))
+(package! cider
+          :recipe (:fetcher github
+                   :repo "clojure-emacs/cider"
+                   :commit "200c88adb1314b5811ba749af42ffb6817c1ca1b"
+                   :files ("*.el" (:exclude ".dir-locals.el"))
+                   :old-names (nrepl)))
+(package! clj-refactor
+          :recipe (:fetcher github
+                   :repo "clojure-emacs/clj-refactor.el"
+                   :commit "3d5d1fbf28bfcc00f917cd96d6784968dcbbc962"))
 (package! flymd)
 (package! lispyville)
 (package! sort-words)
