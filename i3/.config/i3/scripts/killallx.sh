c4e0e24a849f15f7ae21c09cf9c79d95e5d04c25
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-06-08 22:48:52 -0300

    i3: Add program names to killallx.sh

--- i3/.config/i3/scripts/killallx.sh
+++ i3/.config/i3/scripts/killallx.sh
@@ -5,11 +5,13 @@ WIN_PIDS=$(echo "${WIN_LIST}" | awk '{ print $3 }' | sort -u | paste -s -d' ' -)
 
 echo "${WIN_LIST}" | awk '{ print $1 }' | xargs -P0 -L1 wmctrl -ic
 
-(i3-nagbar -m "Waiting all programs to exit..." \
+OPEN_PROGRAMS=$(ps -o comm= -p ${WIN_PIDS} | paste -s -d',' -)
+
+(i3-nagbar -m "Waiting programs to exit: ${OPEN_PROGRAMS}" \
     -b "Kill them!" "kill -9 ${WIN_PIDS}" \
     -B "Finish them!" "kill ${WIN_PIDS}") &
 
 while true; do
-    ps -p ${WIN_PIDS} || break
+    ps -p ${WIN_PIDS} >/dev/null || break
     sleep 0.1
 done
