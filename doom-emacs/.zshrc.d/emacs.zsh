c509f261b2612ed23eba621c1fd0cc1fe20c2b37
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-06-17 19:20:17 -0300

    doom-emacs: Set correct EDITOR when using Emacs term

--- doom-emacs/.zshrc.d/emacs.zsh
+++ doom-emacs/.zshrc.d/emacs.zsh
@@ -6,6 +6,12 @@ path=(
   ${path}
 )
 
+if [[ -n "${EMACS}" ]]; then
+  VISUAL="emacsclient"
+  EDITOR="${VISUAL}"
+  GIT_EDITOR="${EDITOR}"
+fi
+
 alias em="run-bg emacs"
 alias et="TERM=xterm-24bit emacs -nw"
 
