999a898cbf13fbed203a922c36e90f8e363f63c6
Thiago Kenji Okada (Nubank) <thiago.okada@nubank.com.br>
2019-07-01 11:53:01 -0300

    spacemacs: Remove it

--- spacemacs/.zshrc.d/emacs.zsh
+++
@@ -1,6 +0,0 @@
-ZIT_MODULES_PATH="${HOME}" zit-in "https://github.com/syl20bnr/spacemacs#develop" ".emacs.d"
-
-alias em="run-bg emacs"
-alias et="emacs -nw"
-
-em() { close-fd emacsclient -a '' -nqc "${@}" }
