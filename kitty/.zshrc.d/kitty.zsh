cbd8a5b02b21dccfefd0fd6efff69d4058d899a4
Thiago Kenji Okada <thiagokokada@gmail.com>
2019-10-01 11:14:12 -0300

    kitty: Only eval kitty when it exists

--- kitty/.zshrc.d/kitty.zsh
+++ kitty/.zshrc.d/kitty.zsh
@@ -1,13 +1,13 @@
-_kitty() {
-  local src
-  # Send all words up to the word the cursor is currently on
-  src=$(printf "%s " "${(@)words[1,$CURRENT]}" | kitty +complete zsh)
-  if [[ $? == 0 ]]; then
-    eval ${src}
-  fi
-}
-
 if [[ "${TERM}" = "xterm-kitty" ]]; then
+  _kitty() {
+    local src
+    # Send all words up to the word the cursor is currently on
+    src=$(printf "%s " "${(@)words[1,$CURRENT]}" | kitty +complete zsh)
+    if [[ $? == 0 ]]; then
+      eval ${src}
+    fi
+  }
+
   alias icat="kitty +kitten icat"
   alias sshk="kitty +kitten ssh"
   alias d="kitty +kitten diff"
