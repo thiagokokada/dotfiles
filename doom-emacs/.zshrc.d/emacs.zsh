23dfe3af75ceac949111d152606ed724b239059d
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-04 00:09:25 -0300

    doom-emacs: Add install-clojure-lsp script

--- doom-emacs/.zshrc.d/emacs.zsh
+++ doom-emacs/.zshrc.d/emacs.zsh
@@ -18,6 +18,14 @@ alias em="GDK_BACKEND=x11 run-bg emacs"
 alias et="TERM=xterm-24bit emacs -nw"
 alias ec="GDK_BACKEND=x11 close-fd emacsclient -a '' -nqc"
 
+install-clojure-lsp() {
+  local clojure_lsp_path="${HOME}/.local/bin/clojure-lsp"
+  local releases_url="https://api.github.com/repos/snoe/clojure-lsp/releases/latest"
+  local download_url="$(curl -s "${releases_url}" | jq -r '[.assets[]][0].browser_download_url')"
+  curl -L "${download_url}" -o "${clojure_lsp_path}"
+  chmod +x "${clojure_lsp_path}"
+}
+
 EMACS_PATH="${HOME}/.emacs.d"
 
 UPGRADE_CMDS+="doom upgrade -f"
