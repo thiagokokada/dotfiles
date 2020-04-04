6ab3ead125ca68d1fc0b972ce3e79b56e5527813
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-04 00:40:29 -0300

    doom-emacs: Update workaround for bug in completion

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -142,36 +142,8 @@
        (:prefix ("r" . "repl")
          "i" #'cider-interrupt)))))
 
-(after! cider-mode
-  (add-hook 'company-completion-started-hook 'ans/set-company-maps)
-  (add-hook 'company-completion-finished-hook 'ans/unset-company-maps)
-  (add-hook 'company-completion-cancelled-hook 'ans/unset-company-maps)
-
-  (defun ans/unset-company-maps (&rest unused)
-    "Set default mappings (outside of company).
-    Arguments (UNUSED) are ignored."
-    (general-def
-      :states 'insert
-      :keymaps 'override
-      "<up>" nil
-      "<down>" nil
-      "C-j" nil
-      "C-k" nil
-      "RET" nil
-      [return] nil))
-
-  (defun ans/set-company-maps (&rest unused)
-    "Set maps for when you're inside company completion.
-    Arguments (UNUSED) are ignored."
-    (general-def
-      :states 'insert
-      :keymaps 'override
-      "<down>" 'company-select-next
-      "<up>" 'company-select-previous
-      "C-j" 'company-select-next
-      "C-k" 'company-select-previous
-      "RET" 'company-complete
-      [return] 'company-complete)))
+;; Workaround bug in completion
+(add-hook! cider-repl-mode #'evil-normalize-keymaps)
 
 ;; elisp
 (add-hook! emacs-lisp-mode
