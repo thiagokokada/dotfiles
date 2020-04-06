2942c28c2f989347b9749dec7c3b621d13e97844
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-06 15:49:42 -0300

    doom-emacs: Return Clojure company hacks

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -130,7 +130,36 @@
          "i" #'cider-interrupt)))))
 
 ;; Workaround bug in completion
-(add-hook! cider-repl-mode #'evil-normalize-keymaps)
+(after! cider-mode
+  (add-hook 'company-completion-started-hook 'ans/set-company-maps)
+  (add-hook 'company-completion-finished-hook 'ans/unset-company-maps)
+  (add-hook 'company-completion-cancelled-hook 'ans/unset-company-maps)
+
+  (defun ans/unset-company-maps (&rest unused)
+    "Set default mappings (outside of company).
+    Arguments (UNUSED) are ignored."
+    (general-def
+      :states 'insert
+      :keymaps 'override
+      "<up>" nil
+      "<down>" nil
+      "C-j" nil
+      "C-k" nil
+      "RET" nil
+      [return] nil))
+
+  (defun ans/set-company-maps (&rest unused)
+    "Set maps for when you're inside company completion.
+    Arguments (UNUSED) are ignored."
+    (general-def
+      :states 'insert
+      :keymaps 'override
+      "<down>" 'company-select-next
+      "<up>" 'company-select-previous
+      "C-j" 'company-select-next
+      "C-k" 'company-select-previous
+      "RET" 'company-complete
+      [return] 'company-complete)))
 
 (after! clj-refactor
   ;; cljr-find-usages is slow and broken, mostly useless
