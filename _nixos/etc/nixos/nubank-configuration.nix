82ec92f20a880e43d0247c73a568a0bf85f4a8dc
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-08 12:18:00 -0300

    _nixos: Use openfortivpn

--- _nixos/etc/nixos/nubank-configuration.nix
+++ _nixos/etc/nixos/nubank-configuration.nix
@@ -5,6 +5,18 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
+    (openfortivpn.overrideAttrs (oldAttrs: rec {
+      pname = "openfortivpn";
+      version = "1.13.3";
+      name = "${pname}-${version}";
+
+      src = fetchFromGitHub {
+        owner = "adrienverge";
+        repo = "${pname}";
+        rev = "2d89a16943ed043703b9218da4fc2f8ca02d511c";
+        sha256 = "1y3b3zwzig520nyky7xnr0l0zf68i4w698bysyngpkada14d8dv3";
+      };
+    }))
     apacheKafka
     awscli
     boot
