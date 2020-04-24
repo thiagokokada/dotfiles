d5269a1019d4d096264f7eeaa77dd0099a02b7e9
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-24 13:00:36 -0300

    doom-emacs: Improve clojure-lsp workaround

--- doom-emacs/.config/doom/autoload.el
+++ doom-emacs/.config/doom/autoload.el
@@ -3,30 +3,55 @@
 ;;;###autoload
 (defun user/unset-company-maps (&rest unused)
   "Set default mappings (outside of company).
-  Arguments (UNUSED) are ignored."
+   Arguments (UNUSED) are ignored."
   (general-def
     :states 'insert
     :keymaps 'override
-    "<up>" nil
     "<down>" nil
-    "C-j" nil
-    "C-k" nil
-    "RET" nil
-    [return] nil))
+    "<up>"   nil
+    "RET"    nil
+    [return] nil
+    "C-n"    nil
+    "C-p"    nil
+    "C-j"    nil
+    "C-k"    nil
+    "C-h"    nil
+    "C-u"    nil
+    "C-d"    nil
+    "C-s"    nil
+    "C-S-s"  (cond ((featurep! :completion helm) nil)
+                   ((featurep! :completion ivy)  nil))
+    "C-SPC"   nil
+    "TAB"     nil
+    [tab]     nil
+    [backtab] nil))
 
 ;;;###autoload
 (defun user/set-company-maps (&rest unused)
   "Set maps for when you're inside company completion.
-  Arguments (UNUSED) are ignored."
+   Arguments (UNUSED) are ignored."
   (general-def
     :states 'insert
     :keymaps 'override
     "<down>" #'company-select-next
     "<up>" #'company-select-previous
-    "C-j" #'company-select-next
-    "C-k" #'company-select-previous
     "RET" #'company-complete
-    [return] #'company-complete))
+    [return] #'company-complete
+    "C-w"     nil  ; don't interfere with `evil-delete-backward-word'
+    "C-n"     #'company-select-next
+    "C-p"     #'company-select-previous
+    "C-j"     #'company-select-next
+    "C-k"     #'company-select-previous
+    "C-h"     #'company-show-doc-buffer
+    "C-u"     #'company-previous-page
+    "C-d"     #'company-next-page
+    "C-s"     #'company-filter-candidates
+    "C-S-s"   (cond ((featurep! :completion helm) #'helm-company)
+                    ((featurep! :completion ivy)  #'counsel-company))
+    "C-SPC"   #'company-complete-common
+    "TAB"     #'company-complete-common-or-cycle
+    [tab]     #'company-complete-common-or-cycle
+    [backtab] #'company-select-previous))
 
 ;;;###autoload
 (defun user/cider-eval-and-run-ns-tests ()
