3d21569dda86b24a1fe1dd27a7ec524f549b73d7
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-04-28 09:35:13 -0300

    tmux: Fix plugin setup

--- node/.zshrc.d/node.zsh
+++ node/.zshrc.d/node.zsh
@@ -1,5 +1,6 @@
 export NPM_PACKAGES="${HOME}/.npm-packages"
 export NODE_PATH="${NPM_PACKAGES}/lib/node_modules:${NODE_PATH}"
+
 manpath=(
   ${NPM_PACKAGES}/share/man
   ${manpath}
--- tmux/.zshrc.d/tmux.zsh
+++ tmux/.zshrc.d/tmux.zsh
@@ -1,5 +1,8 @@
 ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
-  zit-in "https://github.com/seebi/tmux-colors-solarized" "tmux-colors-solarized" \
-  zit-in "https://github.com/tmux-plugins/tmux-pain-control" "tmux-pain-control" \
-  zit-in "https://github.com/tmux-plugins/tmux-prefix-highlight" "tmux-prefix-highlight" \
+  zit-in "https://github.com/seebi/tmux-colors-solarized" "tmux-colors-solarized"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
+  zit-in "https://github.com/tmux-plugins/tmux-pain-control" "tmux-pain-control"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
+  zit-in "https://github.com/tmux-plugins/tmux-prefix-highlight" "tmux-prefix-highlight"
+ZIT_MODULES_PATH="${HOME}/.tmux/plugins" \
   zit-in "https://github.com/tmux-plugins/tmux-sensible" "tmux-sensible"
