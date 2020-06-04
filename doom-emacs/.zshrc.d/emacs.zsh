9b0385dde75aa55ccbce7c028b337e5cd93f9c29
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-06-04 19:08:47 -0300

    zsh: Improve Emacs' install scripts

--- doom-emacs/.zshrc.d/emacs.zsh
+++ doom-emacs/.zshrc.d/emacs.zsh
@@ -24,6 +24,8 @@ install-clojure-lsp() {
   local clojure_lsp_path="${HOME}/.local/bin/clojure-lsp"
   local releases_url="https://api.github.com/repos/snoe/clojure-lsp/releases/latest"
   local download_url="$(curl -s "${releases_url}" | jq -r '[.assets[]][0].browser_download_url')"
+  echo "${download_url}"
+
   curl -L "${download_url}" -o "${clojure_lsp_path}"
   chmod +x "${clojure_lsp_path}"
 }
@@ -33,11 +35,11 @@ install-clojure-kondo() {
   local releases_url="https://api.github.com/repos/borkdude/clj-kondo/releases/latest"
   # TODO: get static binary using jq's query
   local download_url="$(curl -s "${releases_url}" | jq -r '[.assets[]][1].browser_download_url')"
-  echo "${download_url}"
   local tempdir=$(mktemp -d)
   #trap "rm -rf "${tempdir}"" EXIT
   local tempfile="${tempdir}/clj-kondo.zip"
 
+  echo "${download_url}"
   curl -L "${download_url}" -o "${tempfile}"
   unzip "${tempfile}" -d "${tempdir}"
   mv "${tempdir}/clj-kondo" "${clojure_kondo_path}"
