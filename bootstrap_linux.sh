26cc15256dc255078c2e1135fa03e74fdf364b1b
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-06-22 20:14:47 -0300

    bootstrap_linux.sh: Fix file associations

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -69,8 +69,8 @@ setup_os() {
 
 setup_file_associations() {
   echo "Configuring file associations"
-  xdg-mime default spacefm.desktop inode/directory
-  xdg-mime default org.gnome.gThumb.desktop image/{jpeg,png,gif} 
+  xdg-mime default pcmanfm.desktop inode/directory
+  xdg-mime default org.gnome.gThumb.desktop image/{jpeg,png,gif}
 }
 
 check_dependency "stow"
