51b09535a5694a51cda9df6f0d215b9c893fdcc1
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-08-28 12:11:27 -0300

    _nixos: Use nubank/nixpkgs

--- _nixos/etc/nixos/nubank-configuration.nix
+++ _nixos/etc/nixos/nubank-configuration.nix
@@ -1,18 +1,11 @@
 { config, lib, pkgs, ... }:
-let
-  mkFlutter = opts: pkgs.callPackage (import ./pkgs/flutter.nix opts) { };
-  getPatches = dir:
-    let files = builtins.attrNames (builtins.readDir dir);
-    in map (f: dir + ("/" + f)) files;
-  flutter-nubank = mkFlutter rec {
-    pname = "flutter";
-    channel = "stable";
-    version = "1.20.2";
-    filename = "flutter_linux_${version}-${channel}.tar.xz";
-    sha256Hash = "12j1p3220319411lxbrqfq297fvzjyha1sbscmjpbqc4c4sssxyr";
-    patches = getPatches ./pkgs/patches;
-  };
-in {
+{
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nubank/nixpkgs/archive/master.tar.gz;
+    }))
+  ];
+
   nixpkgs.config = {
     # For Slack/Zoom.
     allowUnfree = true;
