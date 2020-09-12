564ac8dd73e87e450a508b13635546c607710faa
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-09-12 13:10:37 -0300

    i3: Fix imports in i3pyblocks

--- i3/.config/i3pyblocks/config.py
+++ i3/.config/i3pyblocks/config.py
@@ -5,16 +5,16 @@ import logging
 import signal
 from pathlib import Path
 
-import psutil as ps
+import psutil
 
 from i3pyblocks import Runner, types
 from i3pyblocks.blocks import (
-    aiohttp,
-    aionotify,
+    http,
+    inotify,
     datetime,
     i3ipc,
-    psutil,
-    pulsectl,
+    ps,
+    pulse,
     subprocess,
 )
 
@@ -22,7 +22,7 @@ logging.basicConfig(filename=Path.home() / ".i3pyblocks.log", level=logging.DEBU
 
 
 def partitions(excludes=("/boot", "/nix/store")):
-    partitions = ps.disk_partitions()
+    partitions = psutil.disk_partitions()
     return [p for p in partitions if p.mountpoint not in excludes]
 
 
@@ -32,7 +32,7 @@ async def main():
     await runner.register_block(i3ipc.WindowTitleBlock(format=" {window_title:.41s}"))
 
     await runner.register_block(
-        psutil.NetworkSpeedBlock(
+        ps.NetworkSpeedBlock(
             format_up=" {interface:.2s}:  {upload}  {download}",
             format_down="",
             interface_regex="en*|wl*",
@@ -41,29 +41,28 @@ async def main():
 
     for partition in partitions():
         await runner.register_block(
-            psutil.DiskUsageBlock(
+            ps.DiskUsageBlock(
                 format=" {short_path}: {free:.1f}G", path=partition.mountpoint,
             )
         )
 
-    await runner.register_block(psutil.VirtualMemoryBlock(format=" {available:.1f}G"))
+    await runner.register_block(ps.VirtualMemoryBlock(format=" {available:.1f}G"))
 
     await runner.register_block(
-        psutil.SensorsTemperaturesBlock(
-            format="{icon} {current:.0f}°C",
-            icons=((0, ""), (25, ""), (50, ""), (75, "")),
+        ps.SensorsTemperaturesBlock(
+            format="{icon} {current:.0f}°C", icons={0: "", 25: "", 50: "", 75: ""},
         )
     )
 
-    await runner.register_block(psutil.LoadAvgBlock(format=" {load1}"))
+    await runner.register_block(ps.LoadAvgBlock(format=" {load1}"))
 
     await runner.register_block(
-        psutil.SensorsBatteryBlock(
+        ps.SensorsBatteryBlock(
             format_plugged=" {percent:.0f}%",
             format_unplugged="{icon} {percent:.0f}% {remaining_time}",
             format_unknown="{icon} {percent:.0f}%",
             format_no_battery="",
-            icons=((0, ""), (10, ""), (25, ""), (50, ""), (75, "")),
+            icons={0: "", 10: "", 25: "", 50: "", 75: ""},
         )
     )
 
@@ -81,31 +80,31 @@ async def main():
         subprocess.ShellBlock(
             command="xkblayout-state print %s",
             format=" {output}",
-            command_on_click=(
-                (types.MouseButton.SCROLL_UP, "xkblayout-state set +1"),
-                (types.MouseButton.SCROLL_DOWN, "xkblayout-state set -1"),
-            ),
+            command_on_click={
+                types.MouseButton.SCROLL_UP: "xkblayout-state set +1",
+                types.MouseButton.SCROLL_DOWN: "xkblayout-state set -1",
+            },
         )
     )
 
     await runner.register_block(
-        aionotify.BacklightBlock(
+        inotify.BacklightBlock(
             format=" {percent:.0f}%",
             format_no_backlight="",
-            command_on_click=(
-                (types.MouseButton.SCROLL_UP, "light -A 5%"),
-                (types.MouseButton.SCROLL_DOWN, "light -U 5"),
-            ),
+            command_on_click={
+                types.MouseButton.SCROLL_UP: "light -A 5%",
+                types.MouseButton.SCROLL_DOWN: "light -U 5",
+            },
         )
     )
 
     await runner.register_block(
-        pulsectl.PulseAudioBlock(format=" {volume:.0f}%", format_mute=" mute"),
+        pulse.PulseAudioBlock(format=" {volume:.0f}%", format_mute=" mute"),
         signals=(signal.SIGUSR1, signal.SIGUSR2),
     )
 
     await runner.register_block(
-        aiohttp.PollingRequestBlock(
+        http.PollingRequestBlock(
             "https://wttr.in/?format=%c+%t",
             format="{response:.7s}",
             format_error="",
