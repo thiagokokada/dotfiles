f12a0f15f6c8cdd6a26bdddc43c270dd7d61a439
Thiago Kenji Okada <thiagokokada@gmail.com>
2020-12-24 00:50:17 -0300

    libvirt: Improve aux functions

--- libvirt/.zshrc.d/libvirt.zsh
+++ libvirt/.zshrc.d/libvirt.zsh
@@ -1,12 +1,37 @@
-start-win10() {
+# This function generates all the functions to control the VMs managed by
+# libvirt
+make-vm() {
+	local vm_name="${1}"
+	local reserved_guest_cpus="${2:-none}"
+	local vm_functions
+
+	read -r -d '' vm_functions <<EOF
+dump-${vm_name}() {
+	sudo virsh dumpxml "${vm_name}" >! "${DOTFILES_PATH}/_libvirt/${vm_name}.xml"
+}
+
+define-${vm_name}() {
+	sudo virsh define "${DOTFILES_PATH}/_libvirt/${vm_name}.xml"
+}
+
+start-${vm_name}() {
 	sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
-	sudo cset shield --reset
-	sudo cset shield --cpu 2-5 --kthread=on
-	sudo virsh start win10
+	if [[ "${reserved_guest_cpus}" != "none" ]]; then
+		sudo cset shield --reset
+		sudo cset shield --cpu "${reserved_guest_cpus}" --kthread=on
+	fi
+	sudo virsh start "${vm_name}"
 }
 
-
-stop-win10() {
-	sudo virsh shutdown win10
-  sudo cset shield --reset
+stop-${vm_name}() {
+	sudo virsh shutdown "${vm_name}"
+	if [[ "${reserved_guest_cpus}" != "none" ]]; then
+		sudo cset shield --reset
+	fi
+	sudo virsh start "${vm_name}"
 }
+EOF
+	eval "${vm_functions}"
+}
+
+make-vm win10 2-5
