3f16669a81da85994c2b08baa5fa9d04b0da1671
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-06 19:49:27 -0300

    _nixos: Enable CONFIG_PREEMPT in linux-zen

--- _nixos/etc/nixos/overlays/default.nix
+++ _nixos/etc/nixos/overlays/default.nix
@@ -11,6 +11,8 @@ in rec {
 
   linuxZenWMuQSS = pkgs.linuxPackagesFor (pkgs.linux_zen.override {
     structuredExtraConfig = with lib.kernel; {
+      PREEMPT = yes;
+      PREEMPT_VOLUNTARY = lib.mkForce no;
       SCHED_MUQSS = yes;
     };
     ignoreConfigErrors = true;
