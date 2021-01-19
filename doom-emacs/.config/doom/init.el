46e083f3e2982e285674b0d9a9a08edfbece1227
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-19 11:56:20 -0300

    doom-emacs: Remove prescient again

--- doom-emacs/.config/doom/init.el
+++ doom-emacs/.config/doom/init.el
@@ -24,8 +24,7 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        (ivy              ; a search engine for love and life
-        +fuzzy
-        +prescient)
+        +fuzzy)
 
        :ui
        ;;deft              ; notational velocity for Emacs
