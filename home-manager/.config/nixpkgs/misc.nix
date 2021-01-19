d14e19f94e2d67890df27074aa155f0d59b0e5d5
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-19 20:23:05 -0300

    home-manager: Add unstable overlay

--- home-manager/.config/nixpkgs/misc.nix
+++ home-manager/.config/nixpkgs/misc.nix
@@ -1,6 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
+  nixpkgs.overlays = [
+    (self: super:
+      {
+        unstable = import
+          (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") {
+            config = super.config;
+          };
+      }
+    )
+  ];
+
   home = {
     sessionVariables = {
       DOTFILES_PATH = "$HOME/.dotfiles";
