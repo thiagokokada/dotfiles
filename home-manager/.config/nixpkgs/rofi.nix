0ebd77ac92d879f4988242a1b2862452dfda547c
Thiago Kenji Okada <thiagokokada@gmail.com>
2021-01-22 09:45:47 -0300

    home-manager: Backport rofi module

--- home-manager/.config/nixpkgs/rofi.nix
+++ home-manager/.config/nixpkgs/rofi.nix
@@ -1,22 +1,32 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  rofiRefactor = fetchGit {
+    url = "https://github.com/thiagokokada/home-manager";
+    ref = "migrate-rofi-to-rasi";
+    rev = "75fab73330c4fc65c65c14e8e0ba86a4de4a2e2b";
+  };
+in {
+  imports = [ "${rofiRefactor}/modules/programs/rofi.nix" ];
+
+  disabledModules = [ <home-manager/modules/programs/rofi.nix> ];
+
   programs.rofi = {
     enable = true;
     terminal = "${pkgs.kitty}/bin/kitty";
-    package = with pkgs; rofi.override { plugins = [ rofi-calc rofi-emoji ]; };
+    package = with pkgs.unstable; rofi.override { plugins = [ rofi-calc rofi-emoji ]; };
     font = with config.my.fonts; "${gui.package} 14";
     theme = ../../../i3/.config/rofi/custom.rasi;
-    extraConfig = ''
-      rofi.show-icons: true
-      rofi.modi: drun,emoji,ssh
-      rofi.kb-row-up: Up,Control+k
-      rofi.kb-row-down: Down,Control+j
-      rofi.kb-accept-entry: Control+m,Return,KP_Enter
-      rofi.kb-remove-to-eol: Control+Shift+e
-      rofi.kb-mode-next: Shift+Right,Control+Tab,Control+l
-      rofi.kb-mode-previous: Shift+Left,Control+Shift+Tab,Control+h
-      rofi.kb-remove-char-back: BackSpace
-    '';
+    extraConfig = {
+      show-icons = true;
+      modi = "drun,emoji,ssh";
+      kb-row-up = "Up,Control+k";
+      kb-row-down = "Down,Control+j";
+      kb-accept-entry = "Control+m,Return,KP_Enter";
+      kb-remove-to-eol = "Control+Shift+e";
+      kb-mode-next = "Shift+Right,Control+Tab,Control+l";
+      kb-mode-previous = "Shift+Left,Control+Shift+Tab,Control+h";
+      kb-remove-char-back = "BackSpace";
+    };
   };
 }
